b13c3eacda719f14471f3d19c79ceb50c5b08b50
ideeej <ideeej@hotmail.com>
2022-08-10 15:35:47 -0300

    Desafio da maratona em ReactTS

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Product } from './components/Product'
 import './App.css'
 
 function App() {
+const [total, setTotal] = useState(0)
+
   return (
     <div className="App">
       <header>
@@ -11,20 +13,78 @@ function App() {
       </header>
 
       <section>
-        <h2>Título</h2>
         <ul>
           <li>
-            <Product name="Croissant" price="4,99">
-              This is the product description.
+            <h2>Cafés</h2>
+            <Product setTotal={setTotal} name="Americano" price={2.99}>
+              50ml de café americano.
             </Product>
-            <Product name="Coxinha" price="4,99">
-              This is the product description.
+            <Product setTotal={setTotal} name="Expresso - pequeno" price={3.99}>
+              150ml de café expresso.
             </Product>
-            <Product name="Coxinha" price="4,99">
-              This is the product description.
+            <Product setTotal={setTotal} name="Café com leite - pequeno" price={4.99}>
+              150ml de café com leite.
             </Product>
-            <Product name="Coxinha" price="4,99">
-              This is the product description.
+            <Product setTotal={setTotal} name="Expresso - grande" price={6.99}>
+              300ml de café expresso.
+            </Product>
+            <Product setTotal={setTotal} name="Café com leite - grande" price={7.99}>
+              300ml de café com leite.
+            </Product>
+            <Product setTotal={setTotal} name="Cappuccino" price={7.99}>
+              150ml de Cappuccino.
+            </Product>
+            <Product setTotal={setTotal} name="Latte" price={8.99}>
+              150ml de Latte.
+            </Product>
+            <Product setTotal={setTotal} name="Café gelado" price={5.99}>
+              150ml de café gelado.
+            </Product>
+            <Product setTotal={setTotal} name="Americano gelado" price={5.99}>
+              150ml de americano gelado.
+            </Product>
+          
+          </li>
+          <li>
+            <h2>Lanches</h2>
+            <Product setTotal={setTotal} name="Croissant" price={5.99}>
+              De frango
+            </Product>
+            <Product setTotal={setTotal} name="Coxinha" price={4.99}>
+              De frango
+            </Product>
+            <Product setTotal={setTotal} name="Pastel" price={3.99}>
+              De frango e carne
+            </Product>
+            <Product setTotal={setTotal} name="Hamburgão" price={4.99}>
+              Com Hamburger, alface, tomate, maionese caseira, milho, ervilha, queijo e presunto
+            </Product>
+          </li>
+          <li>
+            <h2>Bebidas</h2>
+            <Product setTotal={setTotal} name="Água mineral" price={1.99}>
+              Com gás ou sem gás.
+            </Product>
+            <Product setTotal={setTotal} name="Refrigerante lata" price={2.99}>
+              Coca-Cola, Fanta, Sprite, Guaraná, Laranjinha, Água da Serra
+            </Product>
+            <Product setTotal={setTotal} name="Refrigerante 600ml" price={4.99}>
+              Coca-Cola, Fanta, Sprite, Guaraná, Laranjinha, Água da Serra
+            </Product>
+            <Product setTotal={setTotal} name="Refrigerante 1l" price={5.99}>
+              Coca-Cola, Fanta, Sprite, Guaraná, Laranjinha, Água da Serra
+            </Product>
+            <Product setTotal={setTotal} name="Suco classic 300ml" price={6.99}>
+              Laranja, limão, maçã, morango e goiaba
+            </Product>
+            <Product setTotal={setTotal} name="Suco jumbo 500ml" price={7.99}>
+              Laranja, limão, maçã, morango e goiaba
+            </Product>
+          </li>
+          <li>
+            <h2>SEU PEDIDO</h2>
+            <Product name="TOTAL" price={total}>
+              Soma do seu pedido!
             </Product>
           </li>
         </ul>
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,15 +1,33 @@
+import { useState } from 'react'
 import './Product.css'
 
 export const Product = props => {
+    const [quantity, setQuantity] = useState(0)
     return (
         <div className="product">
             <div className="details">
-                <h3>{props.name}</h3>
+                <h3>{!props.setTotal? "" : `${quantity}x`} {props.name}</h3>
                 <p className="description">
                     {props.children}
                 </p>
-            </div>  
-            <strong className="price">R${props.price}</strong>
+                <button style={{display: !props.setTotal ? 'none' : 'inline' }} onClick={
+                    () => {
+                        setQuantity((quantity) => quantity + 1)
+                        props.setTotal((total) => total+props.price)
+                    }
+                } className="adicionar">+</button>
+                <button style={{display: !props.setTotal ? 'none' : 'inline' }} onClick={
+                    () => {
+                        if (quantity <= 0){return}
+
+                        setQuantity((quantity) => quantity - 1)
+                        props.setTotal((total) => total-props.price)
+                    }
+                } className="adicionar">-</button>
+            </div>
+            <strong className="price">
+                R${props.price.toFixed(2)>0?props.price.toFixed(2):0}
+            </strong>
         </div>
     )
 }
